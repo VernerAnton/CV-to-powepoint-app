4ec32d53de12b70e42ae3d54281b5cfb3f83b3aa
VernerAnton <105201748+VernerAnton@users.noreply.github.com>
2025-11-08 16:41:00 +0000

    fix: Correct VITE_API_KEY declaration in vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_API_KEY: string;
+  readonly VITE_API_KEY: string;AIzaSyAs4uXPOHdEqh7WlYU9KA1ACfRuBeFJt6o
 }
 
 interface ImportMeta {
